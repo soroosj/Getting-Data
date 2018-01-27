a2f36fc25669d2812c5088767e881b23545fab1f
Joel Soroos <soroosj@yahoo.com>
2018-01-27 12:52:46 -0600

    Addl comments

--- natgas.r
+++ natgas.r
@@ -1,5 +1,6 @@
-#download the raw file
+#define path to raw file
    community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+#download the raw file
    download.file(community_url,"natgas.xlsx",method="curl")
 #load file into R
    dat<-read.xlsx("natgas.xlsx", rows = 18:23, cols = 7:15)
