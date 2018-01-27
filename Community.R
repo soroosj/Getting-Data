8324360dc710e4a9ebfeaa48deb2ca0d99f35351
Joel Soroos <soroosj@yahoo.com>
2018-01-27 11:21:16 -0600

    Remove tibble

--- Community.R
+++ Community.R
@@ -2,10 +2,9 @@
    community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
    download.file(community_url,"community.csv",method="curl")
 #load file into R
-   community_raw<-read.csv("community.csv")
-#create data table
-   community <- as_tibble(community_raw)
+   community<-read.csv("community.csv")
 #filter on rows in which property value > $1m
    community_filter <- filter(community, VAL==24)
 #count rows in which property value >$1m
    summarize(community_filter,n())
+   
