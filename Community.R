69b30fc81ad340aa339975debd99732767c8bb23
Joel Soroos <soroosj@yahoo.com>
2018-01-27 11:01:43 -0600

    Add comments

--- Community.R
+++ Community.R
@@ -1,5 +1,12 @@
-community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-community_raw<-read.csv("community.csv")
-community <- tbl_df(community_raw)
-filter(community, VAL==24)
-summarize(community_filter,n())
+#download the file
+   community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+   download.file(community_url,"community.csv",method="curl")
+#load file into R
+   community_raw<-read.csv("community.csv")
+#create data table
+   community <- tbl_df(community_raw)
+#filter on rows in which property value > $1m
+   filter(community, VAL==24)
+#count rows in which property value >$1m
+   summarize(community_filter,n())
+   
