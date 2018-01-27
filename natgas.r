dda81b18625e8aa4dd66325af951e845f7164247
Joel Soroos <soroosj@yahoo.com>
2018-01-27 12:48:43 -0600

    First commit

---
+++ natgas.r
@@ -0,0 +1,8 @@
+#download the file
+   community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+   download.file(community_url,"natgas.xlsx",method="curl")
+#load file into R
+   dat<-read.xlsx("natgas.xlsx", rows = 18:23, cols = 7:15)
+#calculate answer
+   sum(dat$Zip*dat$Ext,na.rm=T)
+   
