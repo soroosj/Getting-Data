50e7f15059a6c6da26a57ac4d5d6b5c1ca4b9a13
Joel Soroos <soroosj@yahoo.com>
2018-01-28 17:38:07 -0600

    Add correct count function

--- restaurant.r
+++ restaurant.r
@@ -2,11 +2,17 @@
    restaurant_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
 #download the raw file
    download.file(restaurant_url,"restaurant.xml",method="curl")
-   library(XML)
 #load file into R
+   library(XML)
+   library(dplyr)
    doc<-xmlTreeParse("restaurant.xml",useInternal=TRUE)
-#calculate answer
+#identify top node
    rootNode <- xmlRoot(doc)
+#parse out zip codes
    zipcode<-xpathSApply(rootNode,"//zipcode",xmlValue)
-   n_distinct(zipcode)
+#filter restaurants for zipcode 21231
+   zipcode_filter<- zipcode[which(zipcode=="21231")]
+#count restaurants for zipcode 21231
+   sum(complete.cases(zipcode_filter))
+   
    
