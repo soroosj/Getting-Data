05a323ce6f3ba6e3164d342a12db51490730fb68
Joel Soroos <soroosj@yahoo.com>
2018-01-28 20:37:34 -0600

    Correct errors

--- natgas.r
+++ natgas.r
@@ -1,8 +1,11 @@
-#define path to raw file
+#load xlsx library into R
+   library(xlsx)
+#define path to Excel file
    community_url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-#download the raw file
+#download the Excel file
    download.file(community_url,"natgas.xlsx",method="curl")
-#load file into R
-   dat<-read.xlsx("natgas.xlsx", rows = 18:23, cols = 7:15)
+#load Excel file into R and create dataframe
+   dat<-read.xlsx("natgas.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15, header=TRUE)
 #calculate answer
    sum(dat$Zip*dat$Ext,na.rm=T)
+   
