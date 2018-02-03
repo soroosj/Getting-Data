3e8dfee640cbaf6a6dd88e36e5cbeca11607f708
Joel Soroos <soroosj@yahoo.com>
2018-02-03 16:55:11 -0600

    Addl comments

--- acs.R
+++ acs.R
@@ -1,3 +1,5 @@
+#Coursera Getting and Cleaning Data, Week 2 Quiz, Problems 2-3
+
 #Install sqldf package to source sqlf function
 library(sqldf)
 
@@ -7,8 +9,11 @@ download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.cs
 #Load ACS file into R dataframe
 acs<-read.csv("acs.csv")
 
-#Query probability weights for ages greater than 50
+#Question 2 - query probability weights for ages greater than 50
 sqldf("SELECT pwgtp1
       FROM acs
       WHERE AGEP < 50")
 
+#Question 3 - identify equivalent SQL function for R function unique (acs$AGEP)
+sqldf("SELECT DISTINCT AGEP 
+      FROM acs")
