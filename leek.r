20036bc7427c3c7ccc2eb3bf87c9992af8741aa5
Joel Soroos <soroosj@yahoo.com>
2018-02-07 20:16:18 -0600

    Additional blank lines for clarity

--- leek.r
+++ leek.r
@@ -1,9 +1,12 @@
-#load httr library into R
+#load XML library
 library(XML)
+
 #define path to HTML file
 url<-"http://biostat.jhsph.edu/~jleek/contact.html"
-#download the HTML file
+
+#download the HTML file to a character vector
 doc <- readLines(url)
+
 #calculate number of characters per code line
 nchar(doc[10])
 nchar(doc[20])
