4c1c8b5a1d06d3c8fa6d3387a0cbcaffb2209ddb
Joel Soroos <soroosj@yahoo.com>
2018-02-10 10:40:49 -0600

    Add sum statement

--- climate.r
+++ climate.r
@@ -1,14 +1,11 @@
-#load XML library
+#load readr library
 library(readr)
 
 #define path to fixed width file
 url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
 
-#download the HTML file to a character vector
-read_fwf(url,skip=3, fwf_positions(28,32))
+#download the fixed width file to a double vector
+dat<-read_fwf(url,skip=4, fwf_positions(28,32))
 
-#calculate number of characters per code line
-nchar(doc[10])
-nchar(doc[20])
-nchar(doc[30])
-nchar(doc[100])
+#sum the field
+sum(dat)
