d689472f57adb21694069a677602c8c993cbc4d0
Joel Soroos <soroosj@yahoo.com>
2018-02-11 07:00:23 -0600

    Simplify code

--- repo.r
+++ repo.r
@@ -1,11 +1,17 @@
+# install httr library to source from API
 library(httr)
+# install jsonlite library to convert JSON format to dataframe
 library(jsonlite)
 
-# 4. Use API
+# extract document from Github API
 req <- GET("https://api.github.com/users/jtleek/repos")
 stop_for_status(req)
 doc<-content(req)
-doc2<-toJSON(dat)
-doc3<-fromJSON(doc2)
-doc4<-doc3[,c(doc3$name,doc3$created_at)]
-doc4[doc3$name=="datasharing",]
+
+#convert API format to JSON
+json1<-toJSON(doc)
+#convert JSON format to dataframe
+json2<-fromJSON(json1)
+
+#select required data
+subset(json2,name=="datasharing",created_at)
