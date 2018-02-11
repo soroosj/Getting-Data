0d0c01706a2aea9ff7a4e1d28ad4b3fae11c1a30
Joel Soroos <soroosj@yahoo.com>
2018-02-11 06:21:20 -0600

    JSON converts

--- repo.r
+++ repo.r
@@ -4,4 +4,8 @@ library(jsonlite)
 # 4. Use API
 req <- GET("https://api.github.com/users/jtleek/repos")
 stop_for_status(req)
-content(req)
+doc<-content(req)
+doc2<-toJSON(dat)
+doc3<-fromJSON(doc2)
+doc4<-doc3[,c(doc3$name,doc3$created_at)]
+doc4[doc3$name=="datasharing",]
