65f472f6e5dfb43c0c6afbdc3209b6feec6e35c9
Joel Soroos <soroosj@yahoo.com>
2018-02-24 10:13:40 -0600

    Additional documentation

--- GettingData.W4.Q3.R
+++ GettingData.W4.Q3.R
@@ -9,7 +9,11 @@
 #2b.load file(s) to R
    gdp<-read.csv("gdp.csv", skip = 5, header = FALSE, nrows=190)
 
-#3b. calculate mean
+#3. eliminate non-U.S. symboles
    gdp$V4 <- str_replace_all(gdp$V4,"\\\\","")
+   
+#4a. Filter country names beginning with United
    united <- grep("^United",gdp$V4) 
+   
+#4b.  Count country names beginning with United
    length(united)
