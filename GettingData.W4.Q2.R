a1c77f04479a146a3e0af053fcbddbfbd35fa990
Joel Soroos <soroosj@yahoo.com>
2018-02-24 09:28:48 -0600

    Update

--- GettingData.W4.Q2.R
+++ GettingData.W4.Q2.R
@@ -7,7 +7,7 @@
    download.file(gdp_url,"gdp.csv",method="curl")
 
 #2b.load file(s) to R
-   gdp<-read.csv("gdp.csv", skip = 5, header = FALSE, na.strings = c("","NA"), nrows=190, stringsAsFactors = F)
+   gdp<-read.csv("gdp.csv", skip = 5, header = FALSE, nrows=190)
 
 #3a. remove decimals
    gdp$V5 <- str_replace_all(gdp$V5,",","") %>%
