8e3c3857dcb6d5eb3b5db829ba6dba7873630bf6
Joel Soroos <soroosj@yahoo.com>
2018-03-04 07:13:02 -0600

    Update column headers

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -9,13 +9,23 @@
 #2b.load file(s) to R
    train<-read_fwf("UCI HAR Dataset/Train/X_train.txt", fwf_empty("UCI HAR Dataset/Train/X_train.txt"))
    test<-read_fwf("UCI HAR Dataset/Test/X_test.txt", fwf_empty("UCI HAR Dataset/Test/X_test.txt"))
+   as.tibble(train)
+   as.tibble(test)
    activity_train <-read_delim("UCI HAR Dataset/Train/y_train.txt", delim = " ", col_names = FALSE)
    activity_test <-read_delim("UCI HAR Dataset/Test/y_test.txt", delim = " ", col_names = FALSE)
    activity_labels <- read_fwf("UCI HAR Dataset/activity_labels.txt", fwf_empty("UCI HAR Dataset/activity_labels.txt"))
    subject_train <-read_delim("UCI HAR Dataset/Train/subject_train.txt", delim = " ", col_names = FALSE)
    subject_test <-read_delim("UCI HAR Dataset/Test/subject_test.txt", delim = " ", col_names = FALSE)
-   features_labels <- read_fwf("UCI HAR Dataset/features.txt", fwf_empty("UCI HAR Dataset/features.txt"))
+   features_labels <- read_delim("UCI HAR Dataset/features.txt", delim = " ", col_names = FALSE)
 
+#3a.add column headers to primary files
+   features_labels <- features_labels$X2
+   colnames(train) <- features_labels
+   colnames(test) <- features_labels
+   
+#3b. add activity, subject to primary files
+   train <- cbind (subject_train,activity_train, train)
+   
 #3. simplify to required rows, columns
    gdp_short <- select (gdp, V1, V2, V4, V5)
    education_short <- select (education, CountryCode, Special.Notes)
